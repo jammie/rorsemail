3b517f293f1f9eafa00762a291bf48c85bb2963c
Lucas Brum <lucasbrum7@gmail.com>
2012-10-27 12:22:06 -0200

    Add a new application Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rorsemail::Application.config.secret_token = '9acbcfca64fbabfeb489379f633d6ae7abcd91b759438ba81c2f9a82724db0816310688421f789f78df600a1f8fd04da44f6613e600da33532be9fb14a6493a8'
